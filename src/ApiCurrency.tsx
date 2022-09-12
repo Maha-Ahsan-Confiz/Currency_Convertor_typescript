4f4a8fc7d66970969e19dd4b6a0e63861c366155
Maha Ahsan <68153005+Maha-Ahsan@users.noreply.github.com>
2022-09-12 16:33:58 +0500

    Sec comit

--- src/ApiCurrency.tsx
+++ src/ApiCurrency.tsx
@@ -2,7 +2,15 @@ import { useEffect, useState } from "react"
 
 const ApiCurrency = () => {
   const [loading, setLoading] = useState(true)
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<{
+success: boolean,
+timestamp: number,
+base: string,
+date: string,
+rates: {
+    [key:string]:string
+}   
+}>(null)
 
   const fetchApi = () => {
     fetch('https://haseeb-apis.herokuapp.com/api/currencies/latest?base=PKR') 
