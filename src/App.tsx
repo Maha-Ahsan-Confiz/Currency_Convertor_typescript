f7f7e3da215ac44f64558ad45d975ec787284f67
Maha Ahsan <68153005+Maha-Ahsan@users.noreply.github.com>
2022-09-12 16:30:53 +0500

    Currency_type

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+
+
+
+import {CurrencyInput} from './CurrencyInput';
+import {useState,useEffect} from "react";
+import ApiCurrency from './ApiCurrency';
+import './style.css'
 function App() {
+
+
+  const [amount1,setAmount1]=useState(1);
+  const [amount2,setAmount2]=useState(1);
+  const [currency1,setCurrency1]=useState('USD');
+  const [currency2,setCurrency2]=useState('PKR');
+  const [rates,setRates]=useState<{[key:string]:string}>();
+
+
+  const { loading, data } = ApiCurrency()
+  console.log("Fetching data from api=",data,loading)
+
+  useEffect(()=>{
+     if (loading !='true' && data!=null){
+      // console.log("In func",data)
+      setRates(data.rates)
+     }
+    },[data])
+
+useEffect(() => {
+  if(!!rates){
+    handleAmount1Change(1)
+  }
+},[rates])
+console.log("Rates=",rates)
+
+function format(number:any){
+  return number.toFixed(4);
+}
+function handleAmount1Change(amount1:any){
+  setAmount2(format(amount1*rates[currency2]/rates[currency1]));
+  setAmount1(amount1);
+}
+
+function handleCurrency1Change(currency1:any){
+  setAmount2(format(amount1*rates[currency2]/rates[currency1]));
+  setCurrency1(currency1);
+}
+
+function handleAmount2Change(amount2:any){
+  setAmount1(format(amount2*rates[currency1]/rates[currency2]));
+  setAmount2(amount2);
+}
+
+function handleCurrency2Change(currency2:any){
+  setAmount1(format(amount2*rates[currency1]/rates[currency2]));
+  setCurrency2(currency2);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+   <h1>Currency Convertor</h1>
+   <CurrencyInput 
+   onAmountChange={handleAmount1Change}
+   onCurrencyChange={handleCurrency1Change}
+   currencies={[Object.keys(rates)]} 
+   amount={amount1} 
+   currency={currency1}/>
+
+   <CurrencyInput
+    onCurrencyChange={handleCurrency2Change}
+     onAmountChange={handleAmount2Change}
+    currencies={[Object.keys(rates)]}  
+    amount={amount2}
+     currency={currency2}/>
     </div>
   );
 }
 
 export default App;
+
